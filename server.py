2cbd4f0ce34d507e6309bc373e08d5ff1e7c43dc
Aiman Farhan <aimanfarhan@DESKTOP-ECU4OAN.localdomain>
2021-01-08 17:49:33 +0800

    showed username on template

--- server.py
+++ server.py
@@ -1,18 +1,10 @@
-from flask import Flask,render_template
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/") # Revisit decorators if you unclear of this syntax
-def index():
-    return render_template('index.html')
+@app.route("/<name>")
+def index(name):
+    name = name.upper()
+    return render_template('index.html', name=name)
 
-@app.route("/lastname")
-def show():
-    return '<h1>Faruk Rahman</h1>'
-
-@app.route ('/user/<username>')
-def show_name(username):
-    username = username.upper()
-    return render_template('index.html',username=username)
-
-if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
-   app.run(debug=True)
+if __name__ == '__main__':
+    app.run()
