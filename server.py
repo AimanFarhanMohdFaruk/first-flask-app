5997110ac362c3fc77881d94a6d582ac3806964e
Aiman Farhan <aimanfarhan@DESKTOP-ECU4OAN.localdomain>
2021-01-08 18:07:51 +0800

    added control flow

--- server.py
+++ server.py
@@ -3,8 +3,9 @@ app = Flask(__name__)
 
 @app.route("/<name>")
 def index(name):
+    signed_in = True
     name = name.upper()
-    return render_template('index.html', name=name)
+    return render_template('index.html', name=name, signed_in=signed_in)
 
 if __name__ == '__main__':
     app.run()
