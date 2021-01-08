1642f979598365280063d8ce9c5819aa8e21acf2
Aiman Farhan <aimanfarhan@DESKTOP-ECU4OAN.localdomain>
2021-01-08 17:42:42 +0800

    passed username to render

--- server.py
+++ server.py
@@ -11,7 +11,8 @@ def show():
 
 @app.route ('/user/<username>')
 def show_name(username):
-    return f"Hello {username}"
+    username = username.upper()
+    return render_template('index.html',username=username)
 
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run(debug=True)
