88b78d84242f00c41e6eb8ca451d2370f26f48ec
Aiman Farhan <aimanfarhan@DESKTOP-ECU4OAN.localdomain>
2021-01-08 16:45:45 +0800

    added new route

--- server.py
+++ server.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return '<h1>Aiman Farhan</h1>'
 
+@app.route("/lastname")
+def show():
+    return '<h1>Faruk Rahman</h1>'
+
 if __name__ == '__main__': # Revisit previous challenge if you're uncertain what this does https://code.nextacademy.com/lessons/name-main/424
    app.run()
