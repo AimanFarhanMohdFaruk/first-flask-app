cf62412ffd9103b6039ab5d1fdaa9c90238cacd6
Aiman Farhan <aimanfaruk98@gmail.com>
2021-01-11 16:19:51 +0800

    template_inheritence

--- server.py
+++ server.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route("/<name>")
-def index(name):
-    signed_in = True
-    name = name.lower()
-    return render_template('index.html', name=name, signed_in=signed_in)
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/contact")
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
