3b08823a41601a26cb467cbc954bc60cccd5010d
Aiman Farhan <aimanfaruk98@gmail.com>
2021-01-11 10:33:23 +0800

    added new branch

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route("/<name>")
 def index(name):
     signed_in = True
-    name = name.upper()
+    name = name.lower()
     return render_template('index.html', name=name, signed_in=signed_in)
 
 if __name__ == '__main__':
